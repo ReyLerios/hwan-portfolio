733322e1399655e35521df6c25ceae78e28fb486
Hanru <90560742+ReyLerios@users.noreply.github.com>
2024-12-25 00:06:21 +0800

    paragraph

--- my-portfolio/src/assets/components/Main/Profile/Profile.jsx
+++ my-portfolio/src/assets/components/Main/Profile/Profile.jsx
@@ -12,13 +12,22 @@ const Profile = () => {
 
   return (
     <div className='profile'>
-        <div className='row mt-5 pt-3'>
+
+        <div className='row mt-5 pb-3 pt-3'>
             <div className='col-sm-6 p-3 text-center justify-content-center'>
                 <img className='rounded-circle p-1' width={120} src={userAvatar} alt='userAvatar.jpg' />
                 <h1 className='pt-3 p-1'>{pName}</h1>
                 <h6>{pJob}</h6>
             </div>
+
+            <div className='col-sm-6'>
+                <p className='text-start p-3'>Twenty-three year old freelance front-end developer and computer science student based in the Philippines.
+                    Together with my swift, effective and problem-solving skills, my stacks are current. I have 2 years of experience of UI/UX 
+                    Designing. I would be grateful for this collaboration with you on this project and convey how my skills will be useful.
+                </p>
+            </div>
         </div>
+
     </div>
   )
 }
