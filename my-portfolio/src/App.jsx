f39424868c872a1b99209fb88c388284ed27bdb5
Hanru <90560742+ReyLerios@users.noreply.github.com>
2024-12-25 18:10:34 +0800

    checker

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navigation from './assets/components/Header/Navigation'
 import Arsenal from './assets/components/Main/Arsenal/Arsenal'
 import Profile from './assets/components/Main/Profile/Profile'
+import Works from './assets/components/Main/Works/Works'
 
 function App() {
 
@@ -13,11 +14,14 @@ function App() {
       </header>
 
       <main>
-        <div className='container-fluid'>
+        <div className='container'>
           <Profile />
         </div>
-        <div className='container-fluid'>
+        <div className='container'>
           <Arsenal />
+        </div>
+        <div className='container'>
+          <Works />
         </div>
       </main>
 
---
+++ my-portfolio/src/assets/components/Main/Works/Works.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Works.css'
+
+const Works = () => {
+  return (
+    <div className='works mt-5 mb-5'>
+        <div className='row gx-1 text-start'>
+            <div className='col-lg-12 p-3'>
+                <h6>Projects</h6>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Works
