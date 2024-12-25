8cddef9bfb3cc8b2af31b8a2799627fbaaa5cf6c
Hanru <90560742+ReyLerios@users.noreply.github.com>
2024-12-25 14:51:50 +0800

    arsenal test

--- my-portfolio/src/assets/components/Main/Arsenal/Arsenal.jsx
+++ my-portfolio/src/assets/components/Main/Arsenal/Arsenal.jsx
@@ -23,8 +23,18 @@ const Arsenal = () => {
     fetch(bootstrapEmblem).then(response => response.json()).then(data => console.log(data))
     fetch(materialEmblem).then(response => response.json()).then(data => console.log(data))
 
+    const jsx = 'React'
+    const ng = 'Angular'
+    const swf = 'Swift'
+    const py = 'Python'
+
+    const exp = 'Express'
+    const lnx = 'Linux'
+    const bs = 'Bootstrap'
+    const mu = 'Material UI'
+
   return (
-    <div className='arsenal'>
+    <div className='arsenal mb-5'>
         <div className='row p-2'>
             <div className='col-sm-4 p-3 text-start'>
                 <h6>Stacks</h6>
@@ -33,29 +43,37 @@ const Arsenal = () => {
         <div className='row gx-1 text-center justify-content-center'>
             <div className='col-lg-3 col-6 col-md-6 col-12 mt-3'>
                 <img width={25} src={reactEmblem} alt='react.svg' />
+                <p className='lang'>{jsx}</p>
             </div>
             <div className='col-lg-3 col-6 col-md-6 col-sm-3 mt-3'>
                 <img width={25} src={angularEmblem} alt='react.svg' />
+                <p className='lang'>{ng}</p>
             </div>
             <div className='col-lg-3 col-6 col-md-6 col-sm-3 mt-3'>
                 <img width={25} src={swiftEmblem} alt='react.svg' />
+                <p className='lang'>{swf}</p>
             </div>
             <div className='col-lg-3 col-6 col-md-6 col-sm-3 mt-3'>
                 <img width={25} src={pythonEmblem} alt='react.svg' />
+                <p className='lang'>{py}</p>
             </div>
         </div>
         <div className='row gx-1 mt-2 text-center justify-content-center'>
             <div className='col-lg-3 col-6 col-md-6 col-12 mt-3'>
                 <img width={25} src={expressEmblem} alt='react.svg' />
+                <p className='lang'>{exp}</p>
             </div>
             <div className='col-lg-3 col-6 col-md-6 col-sm-3 mt-3'>
                 <img width={25} src={linuxEmblem} alt='react.svg' />
+                <p className='lang'>{lnx}</p>
             </div>
             <div className='col-lg-3 col-6 col-md-6 col-sm-3 mt-3'>
                 <img width={25} src={bootstrapEmblem} alt='react.svg' />
+                <p className='lang'>{bs}</p>
             </div>
             <div className='col-lg-3 col-6 col-md-6 col-sm-3 mt-3'>
                 <img width={25} src={materialEmblem} alt='react.svg' />
+                <p className='lang'>{mu}</p>
             </div>
         </div>
     </div>
