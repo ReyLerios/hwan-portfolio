0e2d3026e7f3cdbbf42df35462e14c5161bdb94d
Hanru <90560742+ReyLerios@users.noreply.github.com>
2024-12-31 22:05:11 +0800

    check

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,33 +1,42 @@
+import { useState } from 'react'
 import './App.css'
-import Navigation from './assets/components/Header/Navigation'
-import Arsenal from './assets/components/Main/Arsenal/Arsenal'
-import Profile from './assets/components/Main/Profile/Profile'
-import Works from './assets/components/Main/Works/Works'
+
+const Profile = 'https://res.cloudinary.com/du68ssfzw/image/upload/v1735049873/My%20Portfolio/Image/ztbpdvtpd1h5zzltfdbe.jpg'
 
 function App() {
 
+  fetch(Profile).then(response => response.json()).then(data => console.log(data))
+
+  const name = 'Rey Lerios'
+  const job = 'Front-End Developer'
+
   return (
-    <>
+    <div>
 
-      <header>
-        <Navigation />
-      </header>
+      <div className='container profile mt-3'>
+        <div className='row gx-1 mt-5'>
 
-      <main>
-        <div className='container'>
-          <Profile />
+          <div className='col-sm-6 text-center justify-content-center'>
+            <img className='rounded-circle' src={Profile} width={140} alt='Profile.jpeg' />
+            <h1 className='text-dark pt-3'>{name}</h1>
+            <h6 className='text-body-secondary'>{job}</h6>
+          </div>
+
+          <div className='col-sm-6 text-start mt-4 p-2'>
+            <p className='text-dark'>
+               Twenty-three year old freelance front-end developer and computer science student and computer science student
+               based in the Philippines. Together with my swift, effective and problem-solving skills, my stacks are current.
+               I have 2 years of experience in UI/UX Designing.
+            </p>
+            <p className='text-dark'>
+              I would be grateful for this collaboration with you on this project and convey how my skills will be useful.
+            </p>
+          </div>
+
         </div>
-        <div className='container'>
-          <Arsenal />
-        </div>
-        <div className='container'>
-          <Works />
-        </div>
-      </main>
+      </div>
 
-      <footer></footer>
-
-    </>
+    </div>
   )
 }
 
